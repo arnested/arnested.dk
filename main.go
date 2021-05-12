a34196d1560bb250f0fcfe9fb32774b858349e2a
Arne Jørgensen <arne@arnested.dk>
2021-05-12 22:22:12 +0200

    Add stricter HTTP headers

--- main.go
+++ main.go
@@ -22,7 +22,14 @@ func main() {
 	fs := http.FileServer(webrootFS)
 
 	// Serve static files
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Strict-Transport-Security", "max-age: 10886400; includeSubdomains; preload")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-Content-Type", "nosniff")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'unsafe-inline'")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		fs.ServeHTTP(w, r)
+	})
 
 	addr := ":0"
 	if value, ok := os.LookupEnv("ADDR"); ok {
